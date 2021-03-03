5917dd9833ffd60e3c9fe1aaa5ad631667160d83
alistairgray <alistair.g.gray@gmail.com>
2021-03-03 17:29:15 +1100

    initial commit

---
+++ pr-react-frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <div>
+            <div className="headerText">
+                <h1>prop</h1>
+                <h3>manage your own properties</h3>
+            </div>
+            <div className="floatButton">
+                <button></button>
+            </div>
+        </div>
+    );
+}
---
+++ pr-react-frontend/src/components/PropApp.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import Header from './Header';
+
+function PropApp() {
+    // Declare State
+    const [] = useState(0);
+
+    return (
+        <div>
+            <Nav />
+            <Header />
+        </div>
+    );
+}
+
